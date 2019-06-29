81fc75f13eff039ba8cd5448dc854489deea4222
german <gkiessling95@gmail.com>
2019-06-29 17:23:03 -0300

    Ejercicio del Parcial resuelto

--- Practica/Parcial/prueba.c
+++ Practica/Parcial/prueba.c
@@ -5,8 +5,11 @@
 #include <string.h>
 #include <emmintrin.h>
 
-/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%5 Metodo para calcular primos %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-int primo(int n){  
+#define N 10000 // tamaño arreglo
+//#define FRango 10000 // fin rango primos
+//#define IRango 1 //incio rango primos
+
+int primo(int n){  //retorna 0 cuando no es primo y 1 cuando es primo
     int i, res, mitad;
     i=2;
     res = n > 1;
@@ -16,19 +19,18 @@ int primo(int n){
         res= n%i;
         i++;
     }
-    return res; //retorna 0 cuando no es primo y 1 cuando es primo
+    return res;
 }
-/* %%%%%%%%%%%%%%%%%% Metodo para calcular los tiempos de ejecucion de los procesos %%%%%%%%%%% */
+
 double sampleTime() {
  struct timespec tv;
  clock_gettime(CLOCK_MONOTONIC_RAW, &tv);
  return ((double)tv.tv_sec+((double)tv.tv_nsec)/1000000000.0);
 }
 
-/* %%%%%%%%%%%%%%%%%%%%% Inicio main %%%%%%%%%%%%%%%%%%%% */
 int main(int argc, char *argv[])		
 {
-    int IRango,FRango,bloque,rangoAux;
+ 
 
 	int rank,i,j,esPrimo,world_size;
     /*
@@ -40,9 +42,15 @@ int main(int argc, char *argv[])
     int datos[3];
     /* 1) IRango
        2) FRango
-       3) tamaño bloque 
+       3) tamaño bloque
+       4) Rango aux final 
     */
     
+    /* arreglo donde guardo los primos encontrados uno por proceso */
+    /* se inicializa con un tamaño superior al necesario */
+    //int *arreglo_enviar=(int*)malloc(N*sizeof(int)); //deberia corregir esto
+
+    //sendbuf = (int*)malloc(100 * size * sizeof(int))
     double tiempo=sampleTime();
   
     /* %%%%%%%%%%%%%%%%%%%%%%%%%%% MPI %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5 */
@@ -52,54 +60,46 @@ int main(int argc, char *argv[])
     
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     
-    /* %%%%%%%%%%%%%%%%%%% Solo proceso Root  %%%%%%%%%%%%%%%%%%%%%%%*/
+
     if (rank == 0) {
         /* cargo los rangos */
-        IRango =atoi(argv[1]);
-        FRango =atoi(argv[2]);
-
-        /* tamaño bloques */ 
-        bloque =atoi(argv[3]);
+        int IRango =atoi(argv[1]);
+        int FRango =atoi(argv[2]);
+        //int bloque =atoi(argv)
  
+        /* para trabajar con bloques */
+        int cantElem=(FRango - IRango)+1;
+        /* tamaño bloques */ 
+        int bloque=(cantElem/100);
+        /* cantidad de bloques */ 
+        int cantBloques= cantElem/bloque;
+        /* rango auxiliar numero igual o menos a N*/ 
+        int rangoAux=0;
+
+        //printf("Rank %d con bloque %d y cant bloques %d inicializando buffer\n",rank,bloque,cantBloques);
+        if(cantElem%bloque!=0){
+            rangoAux=(cantBloques*bloque);
+        }else
+        {
+            rangoAux=FRango;
+        }
+
         /* inicio rango */
 	    datos[0]=IRango;
         /* fin rango */
         datos[1]=FRango;
         /* tamaño bloque */
         datos[2]=bloque;
-
-    }
-    
+        /* rango fin aux */
+        datos[3]=rangoAux;
+    } 
+    int *arreglo_enviar=(int*)malloc(N*sizeof(int));
 
     /* %%%%%%%%%%%%%%%%%%%%%5 Broadcast %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-    MPI_Bcast(&datos, 3, MPI_INT, 0, MPI_COMM_WORLD);
-
-    /* %%%% cargo valores del Broadcast %%%%%% */
-    IRango= datos[0];
-    FRango= datos[1];
-    bloque= datos[2];
-
-    /* calculo rango auxiliar  */
-    /* cantidad de elementos en el rango dado */
-    int cantElem=(FRango - IRango)+1;
-    /* cantidad de bloques */ 
-    int cantBloques= cantElem/bloque;
-
-    /* rango auxiliar numero igual o menos a N*/ 
-    if(cantElem%bloque!=0){
-        rangoAux=(cantBloques*bloque);
-    }else
-    {
-        rangoAux=FRango;
-    }
-
-    /* calculo un tamaño para el arreglo que contendra los primos en cada proceso */
-    /* Si la carga esta muy desbalanceda falla */
-    int N=cantElem/2;
-    int *arreglo_enviar=(int*)malloc(N*sizeof(int));
+    MPI_Bcast(&datos, 4, MPI_INT, 0, MPI_COMM_WORLD);
     
     /* posicion de incio de los rank para recorrer */
-    j=IRango+bloque*rank;
+    j=datos[2]*rank;
 
     /* posicion dentro de arreglo_enviar para carga de datos */
     int cantPrimos=0; //la uso para saber cuantos primos encontro el proceso
@@ -108,32 +108,30 @@ int main(int argc, char *argv[])
     int aux;
 
     /* cada proceso recorre bloque/s dentro del rangoAux */
-    if(bloque!=1){
-        while (j<rangoAux)
-        {
-            int count=j+bloque;
-            for (aux = j; aux < count; aux++)
-            {
-                esPrimo=primo(aux);
-                if (esPrimo==1)
-                {
-                    arreglo_enviar[cantPrimos]=aux;
-                    cantPrimos++;
-                }
-            }
-            j+=world_size*bloque;
-        }
-    }else
+    while (j<datos[3])
     {
-        //printf("Bloque tamaño 1 \n");
-        rangoAux=0;
+        int count=j+datos[2];
+        for (aux = j; aux < count; aux++)
+        {
+            esPrimo=primo(aux);
+            if (esPrimo==1)
+            {
+                arreglo_enviar[cantPrimos]=aux;
+                cantPrimos++;
+                if (rank==0){
+                    printf("encontre primo %d =%d \n",cantPrimos, aux);
+                }
+                
+            }
+        }
+        j+=world_size*datos[2];
     }
     
     /* si la cantidad de elementos del rango no era multiplo del tamaño del bloque reviso ultimos elementos */
-    if (FRango!=rangoAux)
+    if (datos[1]!=datos[3])
     {
-        j=rangoAux+rank;
-        while (j<=FRango)
+        j=datos[3]+rank;
+        while (j<datos[1])
         {
             esPrimo=primo(j);
             if (esPrimo==1)
@@ -144,17 +142,18 @@ int main(int argc, char *argv[])
             j+=world_size;
         }
     }
-    /* lo coloque aca porque los gather son bloqueantes */
-    tiempo = sampleTime() - tiempo;
-    printf("Soy el proceso número: %d termine mi calculo de primos y mi tiempo fue de: %f \n",rank,tiempo);
+    printf("proceso %d encontro %d primos \n",rank,cantPrimos);
+    /* redefino reserva de memoria */
+    //arreglo_enviar =(int*) realloc(arreglo_enviar, cantPrimos);
 
-    /* se usa para enviar las candidades de primos que encontro cada proceso al master */
+    /* recvcounts
+	Almacena la cantidad que va a mandar cada proceso: [1,2,3,4,...,n+1]
+	Debe reemplazarse por un gather que recolecte un entero 'cant' por cada worker */
     int *arreglo_cantidades;
-    /*  MPI_Gather  */
+    //printf("size %d \n", world_size);
     /*Operación bloqueante que recolecta, en un proceso, datos de todos los procesos (inclusive
     los que tiene él mismo) asociados a un comunicador. La recolección de segmentos de datos
     es ordenada por número de rank. */
-
     if(rank==0) arreglo_cantidades= (int *) malloc(world_size*sizeof(int));
     MPI_Gather(&cantPrimos,1, MPI_INT, arreglo_cantidades,1, MPI_INT, 0, MPI_COMM_WORLD);
 
@@ -174,29 +173,27 @@ int main(int argc, char *argv[])
 		resultado = (int *) malloc(cant_total*sizeof(int));
 	}
 
-    
-
-    /* envio los arreglos con los primos al master */
     MPI_Gatherv(arreglo_enviar,cantPrimos,MPI_INT,resultado,arreglo_cantidades,arreglo_desplazamientos,MPI_INT,0,MPI_COMM_WORLD);
-    
+
+	/* Solo el proceso root debe imprimir el resultado obtenido
+	El IF puede omitirse ya que para los demas procesos cant_total==0 y el FOR
+	no realizaría ninguna iteración */
 	if(rank == 0){
 		printf("Cantidad de procesos: %d\n",world_size);
-        printf("Tamaño del bloque: %d\n",bloque);
 		printf("Cantidad de enteros recibidos con gatherv: %d\n", cant_total);
-        /* 
 		printf("Arreglo recibido: [");
 		for (i = 0; i < cant_total-1 ; i++)
 		{
 			printf("%d,",resultado[i]);
 		}
-		printf("%d]\n",resultado[i]); */
-    }
+		printf("%d]\n",resultado[i]); //Para evitar la ultima coma en la impresion
+	}
 
     //printf("cant Total %d \n",cant_total);
     
 
     /* %%%%%%%%%%%%%%%%%%%%%% Print primos por procesador %%%%%%%%%%%%%%%%%%%%%%%%% */
-    /* 
+    
     for (i = 0; i < world_size; i++)
     {
         MPI_Barrier(MPI_COMM_WORLD); 
@@ -207,19 +204,18 @@ int main(int argc, char *argv[])
                 printf("%d ",arreglo_enviar[j]);
             }
             printf(" ]\n");
+        }
     }
-    }*/
     
 
 
     
     
-    //tiempo = sampleTime() - tiempo;
-    //printf("Soy el proceso número: %d  mi tiempo fue de: %f \n",rank,tiempo);
+    tiempo = sampleTime() - tiempo;
+    printf("Soy el proceso número: %d  mi tiempo fue de: %f \n",rank,tiempo);
 
 
     MPI_Finalize();
-
 
 	return 0;
 }
